f180129c8d05f6a5d6248ea0a5244951ac5ff514
Daniel Garnier-Moiroux <git@garnier.wf>
2026-04-16 13:23:37 +0200

    demo: add un-secured MCP server

--- mcp-server-appointment/src/main/java/wf/garnier/mcp/security/demo/appointmentmcpserver/SecurityConfiguration.java
+++ mcp-server-appointment/src/main/java/wf/garnier/mcp/security/demo/appointmentmcpserver/SecurityConfiguration.java
@@ -2,14 +2,27 @@ package wf.garnier.mcp.security.demo.appointmentmcpserver;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration {
+
+	@Bean
+	@Order(1)
+	SecurityFilterChain mcpSecurityFilterChain(HttpSecurity http) {
+		return http.securityMatcher("/mcp/**").authorizeHttpRequests(authz -> {
+			authz.anyRequest().permitAll();
+		})
+			.cors(cors -> cors.configurationSource(new AllowAllCorsConfigurationSource()))
+			.csrf(CsrfConfigurer::disable)
+			.build();
+	}
 
 	@Bean
 	SecurityFilterChain webFilterChain(HttpSecurity http) throws Exception {
