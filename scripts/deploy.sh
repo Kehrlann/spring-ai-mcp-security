92ba41b2d1be4bdfdd420fe9ca68745902dede94
Daniel Garnier-Moiroux <git@garnier.wf>
2026-04-20 11:08:25 +0200

    update deploy script so it builds before deploying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,8 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT="$SCRIPT_DIR/.."
 
+"$ROOT/mvnw" -f "$ROOT/pom.xml" package
+
 herokujar --app=mcp-auth-server --jdk=25 "$ROOT/authorization-server/target/authorization-server-0.0.1-SNAPSHOT.jar"
 herokujar --app=mcp-server-appointment --jdk=25 "$ROOT/mcp-server-appointment/target/mcp-server-appointment-0.0.1-SNAPSHOT.jar"
 
